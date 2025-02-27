1719d119b0a297ef52b17655f7fa0548fb07a161
Andrea Coronese <sixpounder@protonmail.com>
2025-02-27 11:53:23 +0100

    fix(colors): More consistent colors across the page

--- src/frontend/components/widgets/CopyToClipboard.tsx
+++ src/frontend/components/widgets/CopyToClipboard.tsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from "react";
 import { useFloating } from "@floating-ui/react-dom";
 
 const CopyToClipboard = (
-  { className, content, label }: Partial<
-    { className: string; content: string; label: string }
+  { className, content, label, alertText }: Partial<
+    { className: string; content: string; label: string; alertText: string }
   >,
 ) => {
   const [tooltipVisible, setTooltipVisible] = useState(false);
@@ -55,14 +55,14 @@ const CopyToClipboard = (
       {tooltipVisible && (
         <div
           ref={refs.setFloating}
-          className="bg-teal-800 text-white text-opacity-90 rounded-lg whitespace-nowrap px-3 py-2"
+          className="dark:bg-slate-500 dark:text-white dark:border-0 bg-white text-gray-800 border border-gray-400 text-opacity-90 rounded-lg whitespace-nowrap px-3 py-2"
           style={{
             position: strategy,
             top: y ?? 0,
             left: x ?? 0,
           }}
         >
-          Copied!
+          {alertText ?? "Copied"}
         </div>
       )}
     </button>
