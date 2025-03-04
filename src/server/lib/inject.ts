76f9e7e5c55e15876ad5ffb6f8b3703fff90c374
Andrea Coronese <sixpounder@protonmail.com>
2025-03-04 11:50:46 +0100

    feat(inject): inject() relax build time bounds
    - Always make it possible to call new() for passed Klass constructor
    - Self-bind methods only if the constructed instance is an object (check at runtime)

--- src/server/lib/inject.ts
+++ src/server/lib/inject.ts
@@ -1,27 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { forEach, isFunction, set } from "lodash-es";
+import { forEach, isFunction, isObject, set } from "lodash-es";
 
 type AnyCtor<T> = new (...args: any[]) => T;
 type AnyFunction<T> = (...args: any[]) => T;
 
 /**
- * Create a new instance of `Klass` by injecting values in its
- * constructor. Also binds its methods to itself so that it is
+ * Create a new instance of `Klass` by injecting `injects` values in its
+ * constructor. If `new Klass` yields an object then this
+ * also binds its methods to itself, so that it is
  * safe to use as a middleware.
  * @param Klass the type to instantiate
  * @param injects the values to inject on the constructor
  * @returns
  */
-export function inject<T extends object>(
+export function inject<T>(
   Klass: AnyCtor<T>,
   ...injects: any[]
 ): T {
   const instance = new Klass(...injects);
-  const fns = Object.getOwnPropertyNames(Klass.prototype)
-    .filter((name) => isFunction(instance[name]) && name !== "constructor");
-  forEach(fns, (name) => {
-    const fn: AnyFunction<any> = instance[name];
-    set(instance, name, fn.bind(instance));
-  });
+  if (isObject(instance)) {
+    const fns = Object.getOwnPropertyNames(Klass.prototype)
+      .filter((name) => isFunction(instance[name]) && name !== "constructor");
+    forEach(fns, (name) => {
+      const fn: AnyFunction<any> = instance[name];
+      set(instance, name, fn.bind(instance));
+    });
+  }
   return instance;
 }
